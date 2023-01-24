f7f74d7662f2607f177d9de49915a1a7bb0cd26d
Mico <m.enriquez4060@hotmail.com>
2023-01-24 09:46:51 -0800

    1-24-2023

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -1,4 +1,19 @@
 $("document").ready(function(){
-     //alert("hi from jQuery! This is a shorter version!");
-     $("div#myDiv").css("background-color","green");
+     let counter = 0;
+     $("div#myDiv p").each(function(){
+          counter++;
+          $(this).prepend(counter + "] ");
+          //code goes here
      });
+
+});
+
+/*
+     let myPTags = document.querySelectorAll("div#myDIv p");
+     console.log(myPTags);
+*/ 
+     /*
+     document.querySelector("div#myDiv").style.backgroundColor="yellow";
+
+     document.querySelector("div#myDiv").innerHTML = "Heres some new text!";
+*/
