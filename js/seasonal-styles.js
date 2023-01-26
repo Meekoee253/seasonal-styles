ac866e3b129a3919e9ff820884ffe3b9c45f3b5f
Mico <m.enriquez4060@hotmail.com>
2023-01-26 10:32:22 -0800

    1

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -1,19 +1,47 @@
 $("document").ready(function(){
-     let counter = 0;
-     $("div#myDiv p").each(function(){
-          counter++;
-          $(this).prepend(counter + "] ");
-          //code goes here
+     $('.seasons a').click(function(e){//find all a tags inside class of seasons
+         e.preventDefault();//stop default submission
+         var season = $(this).attr("href");//contents of href attribute of this element
+         //alert(season);//
+
+         season = season.toLowerCase();
+
+          switch(season){
+
+               case "spring":
+                    $("#logo").attr("src","images/spring.gif");
+
+                    $("html").css("background-color","#2B7129");
+               break;
+               case "summer":
+                    $("#logo").attr("src","images/summer.gif");
+
+                    $("html").css("background-color","#EBA52B");
+               break;
+               case "fall":
+                    $("#logo").attr("src","images/fall.gif");
+
+                    $("html").css("background-color","#A81124");
+               break;
+               case "winter":
+                    $("#logo").attr("src","images/winter.gif");
+
+                    $("html").css("background-color","#005393");
+               break;
+               case "default":
+                    $("#logo").attr("src", "images/four-seasons.gif");
+                    $("html").css("background-color","white");
+                break;
+               
+               
+               default: 
+                    alert("You didnt hit a case!");
+          }
+
+
+
+
+
      });
-
-});
-
-/*
-     let myPTags = document.querySelectorAll("div#myDIv p");
-     console.log(myPTags);
-*/ 
-     /*
-     document.querySelector("div#myDiv").style.backgroundColor="yellow";
-
-     document.querySelector("div#myDiv").innerHTML = "Heres some new text!";
-*/
+ });
+ 
